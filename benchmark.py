1f91eb3944530596e81ce546f1773dc524a294a3
Marcel Martin <marcel.martin@scilifelab.se>
2022-01-18 16:01:24 +0100

    more

--- benchmark.py
+++ benchmark.py
@@ -4,14 +4,16 @@ import sys
 import time
 import subprocess
 
-PROGRAMS = [
-#    "gzip",
-    "igzip",
-    #"pigz",
-    "lzop",
-    "lz4",
-    "zstd",
-]
+
+# pigz
+
+PROGRAMS = {
+    "igzip": range(1, 4),
+    "lzop": [1],
+    "zstd": range(1, 10),
+    "gzip": range(1, 10),
+    "lz4": range(1, 10),
+}
 
 
 def measure(program: str, level: int, path: str) -> (float, float):
@@ -19,7 +21,10 @@ def measure(program: str, level: int, path: str) -> (float, float):
     return pair (compressed size, elapsed wall-clock time)
     """
     start_time = time.time()
-    command = f"{program} -{level} < {path}"
+    if level is not None:
+        command = f"{program} -{level} < {path}"
+    else:
+        command = f"{program} < {path}"
     print("Running", command, file=sys.stderr)
     result = subprocess.run(f"{command} | wc -c", shell=True, capture_output=True)
     size = int(result.stdout)
@@ -30,8 +35,10 @@ def measure(program: str, level: int, path: str) -> (float, float):
 if __name__ == "__main__":
     path = sys.argv[1]
     fsize = os.stat(path).st_size
+    size, elapsed = measure("cat", None, path)
+    print("cat", 0, size, f"{elapsed:.3f}", sep="\t")
     for prog in PROGRAMS:
-        for level in range(1, 10):
+        for level in PROGRAMS[prog]:
             size, elapsed = measure(prog, level, path)
             print(prog, level, size, f"{elapsed:.3f}", sep="\t")
             sys.stdout.flush()
