92ae1128098dda34602108c54a914c6ab55f5169
Marcel Martin <marcel.martin@scilifelab.se>
2022-01-25 16:34:52 +0100

    repeat time measurements

--- benchmark.py
+++ benchmark.py
@@ -16,29 +16,38 @@ PROGRAMS = {
 }
 
 
-def measure(program: str, level: int, path: str) -> (float, float):
+def measure(program: str, level: int, path: str, n: int) -> (float, float):
     """
     return pair (compressed size, elapsed wall-clock time)
     """
-    start_time = time.time()
     if level is not None:
         command = f"{program} -{level} < {path}"
+        args = [program, f"-{level}"]
     else:
         command = f"{program} < {path}"
-    print("Running", command, file=sys.stderr)
-    result = subprocess.run(f"{command} | wc -c", shell=True, capture_output=True)
+        args = [program]
+    print("Measuring size by running", command, file=sys.stderr)
+    result = subprocess.run(f"{command} | wc -c", shell=True, check=True, capture_output=True)
     size = int(result.stdout)
-    elapsed = time.time() - start_time
-    return (size, elapsed)
+
+    elapsed = []
+    for i in range(n):
+        print("Measuring time, iteration", i + 1, file=sys.stderr)
+        with open(path) as f:
+            start_time = time.time()
+            result = subprocess.run(args, check=True, stdin=f, stdout=subprocess.DEVNULL)
+            elapsed.append(time.time() - start_time)
+    print(elapsed, file=sys.stderr)
+    return (size, min(elapsed))
 
 
 if __name__ == "__main__":
     path = sys.argv[1]
     fsize = os.stat(path).st_size
-    size, elapsed = measure("cat", None, path)
-    print("cat", 0, size, f"{elapsed:.3f}", sep="\t")
+    uncompressed, elapsed = measure("cat", None, path, 5)
+    print("cat", 0, uncompressed, f"{elapsed:.3f}", sep="\t")
     for prog in PROGRAMS:
         for level in PROGRAMS[prog]:
-            size, elapsed = measure(prog, level, path)
+            size, elapsed = measure(prog, level, path, 3)
             print(prog, level, size, f"{elapsed:.3f}", sep="\t")
             sys.stdout.flush()
